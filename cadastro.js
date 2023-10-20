ef99657908c7b992f59a2cdd56103e082a364fab
Simões <mariaeduardasimoesal@gmail.com>
2023-10-20 10:13:41 -0300

    arrumando erros

--- cadastro.js
+++ cadastro.js
@@ -1,18 +1,16 @@
 //######################
 //Código de validação de formulário de Cadastro
 function verificar() {
-  let Nome = document.getElementById('nome').value;
-  let DatadeNascimento = document.getElementById('dtnasc').value;
   let CPF = document.getElementById('CPF').value;
   let Nome = document.getElementById('Nome').value;
   let Email = document.getElementById('Email').value;
-  let senha = document.getElementById('Senha').value;
+  let Senha = document.getElementById('Senha').value;
   let Rua = document.getElementById('Rua').value;
   let Numero = document.getElementById('Numero').value;
   let Bairro = document.getElementById('Bairro').value;
-  let Estado = document.getElementById('Cidade').value;
+  let Cidade = document.getElementById('Cidade').value;
  
-  if (!CPF || !Nome || !Email || !Senha || !Rua || !Numero || !Bairro|| !Estado || !Cidade  ) {
+  if (!CPF || !Nome || !Email || !Senha || !Rua || !Numero || !Bairro || !Estado || !Cidade  ) {
     alert("Campo de preenchimento obrigatório");
 
   }else{
